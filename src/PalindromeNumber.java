c722de3f2814cbb47ca10062c5f89aa8d4a04502
Bao Hoai To <tohoaibao@gmail.com>
2020-05-10 21:55:58 +0700

    Palindrome Number

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,23 @@
+
+public class PalindromeNumber {
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int rev = 0;
+        while (x > rev) {
+            rev = rev*10 + x % 10;
+            x /= 10;
+        }
+
+        return x==rev || x==rev/10;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(isPalindrome(123));
+    }
+}
+
