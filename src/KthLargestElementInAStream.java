64c1990fb0c59aad240ed348bb06067bf316e12b
Bao Hoai To <tohoaibao@gmail.com>
2020-05-22 20:10:18 +0700

    Kth Largest Element in a Stream

---
+++ src/KthLargestElementInAStream.java
@@ -0,0 +1,37 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class Solution {
+    Queue<Integer> pq;
+    int k;
+
+    public Solution(int k, int[] nums) {
+        pq = new PriorityQueue<>();
+        this.k = k;
+        for (int num: nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if (pq.size() < k) {
+            pq.add(val);
+        } else if (pq.peek() < val) {
+            pq.poll();
+            pq.add(val);
+        }
+        return pq.peek();
+    }
+
+    public static void main(String[] args) {
+        int k = 3;
+        int[] arr = {4,5,8,2};
+        Solution kthLargest = new Solution(3, arr);
+        System.out.println(kthLargest.add(3));
+        System.out.println(kthLargest.add(5));
+        System.out.println(kthLargest.add(10));
+        System.out.println(kthLargest.add(9));
+        System.out.println(kthLargest.add(4));
+
+    }
+}
