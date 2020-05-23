c3a1d764447f9f9b297eed650d464cb0d2e74bed
Bao Hoai To <tohoaibao@gmail.com>
2020-05-23 15:49:46 +0700

    Implement Trie (Prefix Tree)

---
+++ src/Trie.java
@@ -0,0 +1,93 @@
+class Trie {
+
+    private TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); ++i) {
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar, new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+
+    // search a prefix or whole key in trie and
+    // returns the node where search ends
+    public TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); ++i) {
+            char ch = word.charAt(i);
+            if (node.containsKey(ch)) {
+                node = node.get(ch);
+            } else {
+                return null;
+            }
+        }
+        return node;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+
+    class TrieNode {
+        // R links to node children
+        private TrieNode[] links;
+
+        private final int R = 26;
+
+        private boolean isEnd;
+
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+
+        public boolean containsKey(char ch) {
+            return links[ch - 'a'] != null;
+        }
+
+        public TrieNode get(char ch) {
+            return links[ch - 'a'];
+        }
+
+        public void put(char ch, TrieNode node) {
+            links[ch - 'a'] = node;
+        }
+
+        public void setEnd() {
+            isEnd = true;
+        }
+
+        public boolean isEnd() {
+            return isEnd;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Trie obj = new Trie();
+        obj.insert("apple");
+        obj.insert("app");
+        boolean param_2 = obj.search("apple");
+        boolean param_3 = obj.startsWith("appk");
+        System.out.println(param_2);
+        System.out.println(param_3);
+    }
+}
