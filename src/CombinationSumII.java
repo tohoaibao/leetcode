6996fb5e6189baffdef208c05d8f4c21bbf0210b
Bao Hoai To <tohoaibao@gmail.com>
2020-06-02 20:12:01 +0700

    Combination Sum II

---
+++ src/CombinationSumII.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class CombinationSumII {
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(res, new ArrayList<>(), candidates, target, 0);
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, List<Integer> tempList, int[] nums, int remain, int start) {
+        if (remain == 0) {
+            res.add(new ArrayList<>(tempList));
+        }
+        for (int i = start; i < nums.length; i++) {
+            if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+            if (nums[i] <= remain) {
+                tempList.add(nums[i]);
+                backtrack(res, tempList, nums, remain - nums[i], i + 1);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int candidates[] = {10,1,2,7,6,1,5};
+        int target = 8;
+        List<List<Integer>> res = new CombinationSumII().combinationSum2(candidates, target);
+        System.out.println();
+    }
+}
