b6a3742b9d71b2a06f3ca6d4196193b42091ae80
Bao Hoai To <tohoaibao@gmail.com>
2020-06-10 09:07:57 +0700

    update

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -9,4 +9,28 @@ public class TreeNode {
         this.left = left;
         this.right = right;
     }
+
+    public void printPreorder(TreeNode root, StringBuilder result) {
+        if (root != null) {
+            result.append(root.val + " ");
+            printPreorder(root.left, result);
+            printPreorder(root.right, result);
+        }
+    }
+
+    public void printInorder(TreeNode root, StringBuilder result) {
+        if (root != null) {
+            printInorder(root.left, result);
+            result.append(root.val + " ");
+            printInorder(root.right, result);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        printPreorder(this, result);
+//        printInorder(this, result);
+        return result.toString();
+    }
 }
