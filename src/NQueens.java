011fa5380ebfdf845530a4e263627287dcddb85f
Bao Hoai To <tohoaibao@gmail.com>
2020-06-23 15:45:04 +0700

    N-Queens

---
+++ src/NQueens.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NQueens {
+
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+
+        List<List<String>> res = new ArrayList<>();
+        dfs(board, 0, res);
+
+        return res;
+    }
+
+    private void dfs(char[][] board, int colIndex, List<List<String>> res) {
+        if (colIndex == board.length) {
+            res.add(construct(board));
+            return;
+        }
+        for (int i = 0 ; i < board.length; i++) {
+            if (validate(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                dfs(board, colIndex + 1, res);
+                board[i][colIndex] = '.';
+            }
+        }
+    }
+
+    private boolean validate(char[][] board, int x, int y) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < y; j++) {
+                if (board[i][j] == 'Q' && (x + j == y + i || x + y == i + j || x == i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> res = new ArrayList<>();
+        for (char[] line: board) {
+            res.add(new String(line));
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        List<List<String>> res = new NQueens().solveNQueens(4);
+        for (List<String> lst: res) {
+            for (String line: lst){
+                System.out.println(line);
+            }
+            System.out.println("-----------------");
+        }
+    }
+}
