8bc21b065a45e324475a2bacb147cac4fbd84bd9
Bao Hoai To <tohoaibao@gmail.com>
2020-06-24 14:38:20 +0700

    Add Binary

---
+++ src/AddBinary.java
@@ -0,0 +1,35 @@
+public class AddBinary {
+
+    // https://leetcode.com/problems/add-binary/
+    public String addBinary(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        int sum;
+        while (i >= 0 || j >= 0) {
+            sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                sum += b.charAt(j--) - '0';
+            }
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry > 0) {
+            sb.append(carry % 2);
+        }
+
+        return sb.reverse().toString();
+    }
+
+
+
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1011";
+        System.out.println(new AddBinary().addBinary(a, b));
+    }
+}
