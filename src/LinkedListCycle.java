6b6b59088d67a03f2fde50b896dfd9185503f7d4
Bao Hoai To <tohoaibao@gmail.com>
2020-06-26 13:16:15 +0700

    Linked List Cycle

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,34 @@
+class LinkedListCycle {
+
+    // https://leetcode.com/problems/linked-list-cycle/
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        ListNode n1 = new ListNode(2);
+        ListNode n2 = new ListNode(0);
+        ListNode n3 = new ListNode(-4);
+
+        head.next = n1;
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n1;
+
+        System.out.println(new LinkedListCycle().hasCycle(head));
+    }
+}
