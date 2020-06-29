6a343d025654dd1eb411f60adc96477fb321fccb
Bao Hoai To <tohoaibao@gmail.com>
2020-06-29 10:49:47 +0700

    Min Stack

---
+++ src/MinStack.java
@@ -0,0 +1,56 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+class MinStack {
+
+    /** initialize your data structure here. */
+    Stack<Integer> stack;
+    int min = Integer.MAX_VALUE;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int x) {
+        if (x <= min) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        if (stack.pop() == min) {
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public static void main(String[] args) {
+        MinStack obj = new MinStack();
+        obj.push(-2);
+        obj.push(0);
+        obj.push(-3);
+        System.out.println(obj.getMin()); // return -3
+        obj.pop();
+        System.out.println(obj.top()); // return 0
+        System.out.println(obj.getMin()); // return 2
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
