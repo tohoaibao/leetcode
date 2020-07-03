3bd3c049beb808c9d9464dfe12a1fa5d84d0838c
Bao Hoai To <tohoaibao@gmail.com>
2020-07-03 11:24:21 +0700

    Middle of the Linked List

---
+++ src/MiddleOfTheLinkedList.java
@@ -0,0 +1,30 @@
+class MiddleOfTheLinkedList {
+
+    // https://leetcode.com/problems/middle-of-the-linked-list/
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        ListNode n6 = new ListNode(6);
+
+        head.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+
+        System.out.println(new MiddleOfTheLinkedList().middleNode(head));
+    }
+}
