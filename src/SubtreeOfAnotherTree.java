95c63fd71797ff6be8196771296496118292a160
Bao To Hoai <tohoaibao@gmail.com>
2020-07-24 11:16:08 +0700

    Subtree of Another Tree

---
+++ src/SubtreeOfAnotherTree.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class SubtreeOfAnotherTree {
+
+    // https://leetcode.com/problems/subtree-of-another-tree/
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (isSameTree(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+    }
+
+    public static void main(String[] args) {
+        TreeNode s = new TreeNode(3);
+        s.left = new TreeNode(4);
+        s.right = new TreeNode(5);
+        s.left.left = new TreeNode(1);
+        s.left.right = new TreeNode(2);
+//        s.left.right.left = new TreeNode(0);
+
+        TreeNode t = new TreeNode(4);
+        t.left = new TreeNode(1);
+        t.right = new TreeNode(2);
+
+//        System.out.println(s);
+//        System.out.println(t);
+        System.out.println(new SubtreeOfAnotherTree().isSubtree(s, t));
+    }
+}
