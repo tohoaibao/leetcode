c5e47d92a43a21f70741ba9a2864c2d9b614d4e7
Bao To Hoai <tohoaibao@gmail.com>
2020-07-24 15:19:07 +0700

    Binary Tree Preorder Traversal

---
+++ src/BinaryTreePreorderTraversal.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null) stack.push(node.right);
+            if (node.left != null) stack.push(node.left);
+        }
+
+        return res;
+    }
+
+    private void preorder(TreeNode root, List<Integer> res) {
+        if (root != null) {
+            res.add(root.val);
+            preorder(root.left, res);
+            preorder(root.right, res);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(5);
+
+        System.out.println(Arrays.toString(new BinaryTreePreorderTraversal().preorderTraversal(root).toArray()));
+    }
+}
