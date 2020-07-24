b4ead88f65a51c2c9ecda348f5fc79f7f910c53b
Bao To Hoai <tohoaibao@gmail.com>
2020-07-24 10:16:16 +0700

    Update Reshape the Matrix

--- src/ReshapeTheMatrix.java
+++ src/ReshapeTheMatrix.java
@@ -20,6 +20,24 @@ class ReshapeTheMatrix {
         return res;
     }
 
+    public int[][] matrixReshape2(int[][] nums, int r, int c) {
+        int[][] res = new int[r][c];
+        if (nums.length == 0 || r * c != nums.length * nums[0].length)
+            return nums;
+        int rows = 0, cols = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[0].length; j++) {
+                res[rows][cols] = nums[i][j];
+                cols++;
+                if (cols == c) {
+                    rows++;
+                    cols = 0;
+                }
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[][] nums = {{1,2},{3,4}};
         int r = 1, c = 4;
