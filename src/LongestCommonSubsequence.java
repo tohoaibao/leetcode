53a5405d1f3a7122bf8885ac0a9b9497a36421f3
Bao To Hoai <tohoaibao@gmail.com>
2020-07-24 13:54:13 +0700

    Longest Common Subsequence

---
+++ src/LongestCommonSubsequence.java
@@ -0,0 +1,25 @@
+class LongestCommonSubsequence {
+
+    // https://leetcode.com/problems/longest-common-subsequence/
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n1 = text1.length(), n2 = text2.length();
+        int[][] dp = new int[n1 + 1][n2 + 1];
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+
+    public static void main(String[] args) {
+        String text1 = "abcde", text2 = "ace";
+//        String text1 = "abcde", text2 = "ace";
+//        String text1 = "abc", text2 = "def";
+        System.out.println(new LongestCommonSubsequence().longestCommonSubsequence(text1, text2));
+    }
+}
