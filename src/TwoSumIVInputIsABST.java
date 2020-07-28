2f6f254b6acd00ba6078279bd77482b3d056997a
Bao To Hoai <tohoaibao@gmail.com>
2020-07-28 09:09:01 +0700

    Two Sum IV - Input is a BST

---
+++ src/TwoSumIVInputIsABST.java
@@ -0,0 +1,44 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class TwoSumIVInputIsABST {
+
+    // https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+    public boolean findTarget(TreeNode root, int k) {
+        return find(root, k, new HashSet<>());
+    }
+
+    private boolean find(TreeNode root, int k, Set<Integer> set) {
+        if (root == null) return false;
+        if (set.contains(k - root.val)) return true;
+        set.add(root.val);
+        return find(root.left, k, set) || find(root.right, k, set);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.right = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        int k= 8;
+        System.out.println(new TwoSumIVInputIsABST().findTarget(root, k));
+    }
+}
