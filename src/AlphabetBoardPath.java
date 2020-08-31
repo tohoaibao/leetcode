cfbc003f7cc450447cd5f00b6a3989443cb3d0f5
Bao To Hoai <tohoaibao@gmail.com>
2020-08-31 16:32:11 +0700

    Update Alphabet Board Path

--- src/AlphabetBoardPath.java
+++ src/AlphabetBoardPath.java
@@ -1,24 +1,12 @@
-
-import java.util.HashMap;
-import java.util.Map;
-
 class AlphabetBoardPath {
 
     // https://leetcode.com/problems/alphabet-board-path/
     public String alphabetBoardPath(String target) {
-        String[] board = {"abcde", "fghij", "klmno", "pqrst", "uvwxy", "z"};
-        Map<Character, int[]> map = new HashMap<>();
-        for (int i = 0; i < board.length; ++i) {
-            for (int j = 0; j < board[i].length(); ++j) {
-                map.put(board[i].charAt(j), new int[]{i, j});
-            }
-        }
         int x = 0, y = 0;
         StringBuilder sb = new StringBuilder();
         for (char c : target.toCharArray()) {
-            int[] p = map.get(c);
-            int x1 = p[0];
-            int y1 = p[1];
+            int x1 = (c-'a')/5;
+            int y1 = (c-'a')%5;
             while (x1 < x) {x--; sb.append('U');}
             while (y1 > y) {y++; sb.append('R');}
             while (y1 < y) {y--; sb.append('L');}
