7d010558dd191d24b2327caac727648336f59b6b
To Hoai Bao <tohoaibao@gmail.com>
2020-09-10 13:40:38 +0700

    Update

--- src/ReplaceElementsWithGreatestElementOnRightSide.java
+++ src/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -6,6 +6,7 @@ class ReplaceElementsWithGreatestElementOnRightSide {
     public int[] replaceElements(int[] arr) {
         int max = -1;
         for (int i = arr.length - 1; i >= 0; i--) {
+            // max = Math.max(arr[i], arr[i] = max);
             int temp = arr[i];
             arr[i] = max;
             if (temp > max) {
