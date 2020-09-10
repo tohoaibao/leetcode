4d55ddb78bdaae9f7b11ae05aaf5ffee27ebcd0e
To Hoai Bao <tohoaibao@gmail.com>
2020-09-10 11:12:04 +0700

    Add method Sequential Digits

--- src/SequentialDigits.java
+++ src/SequentialDigits.java
@@ -27,6 +27,24 @@ class SequentialDigits {
         }
     }
 
+    public List<Integer> sequentialDigits2(int low, int high) {
+        int[] allNums = {12,23,34,45,56,67,78,89,
+                123,234,345,456,567,678,789,
+                1234,2345,3456,4567,5678,6789,
+                12345,23456,34567,45678,56789,
+                123456,234567,345678,456789,
+                1234567,2345678,3456789,
+                12345678,23456789,
+                123456789};
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < allNums.length; i++) {
+            if (allNums[i] < low) continue;
+            if (allNums[i] > high) break;
+            res.add(allNums[i]);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int low = 10, high = 1000;
         System.out.println(new SequentialDigits().sequentialDigits(low, high));
